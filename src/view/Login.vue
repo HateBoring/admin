c1339c8e4d7180f91ab53ee1fc317e9bfd3d7b7c
627315114@qq.com <627315114@qq.com>
2019-12-16 10:55:44 +0800

    19-12-16-2

---
+++ src/view/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    login
+  </div>
+</template>
+
+<script>
+import '../../static/js/demo.js'
+import '../../static/js/vendors.js'
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+    }
+  },
+  mounted(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+@import '../../static/css/demo.css';
+@import '../../static/css/vendors.css';
+
+</style>
